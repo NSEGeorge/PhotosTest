04d8d9f16d63c227127e89b9eefd6215f6e6f9ec
Georguy <porox9696@mail.ru>
2016-07-06 12:28:31 +0300

    Initial Commit

---
+++ PhotosTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotosTest
+//
+//  Created by Georgij on 06.07.16.
+//  Copyright © 2016 Georgii Emeljanow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotosTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotosTest
+//
+//  Created by Georgij on 06.07.16.
+//  Copyright © 2016 Georgii Emeljanow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
